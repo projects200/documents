e12953681eb1c59385506addea583a358df76862
JooH <nexon9093@gmail.com>
2025-09-25 22:55:11 +0900

    Update For Develop.sql

--- SQL/For Develop.sql
+++ SQL/For Develop.sql
@@ -46,3 +46,14 @@ CREATE TABLE matches
     CONSTRAINT FK_members_TO_matches_2 FOREIGN KEY (receiver_id)
         REFERENCES members (member_id)
 );
+
+------
+-- 운동 장소 생성시 추가 
+INSERT INTO policy_groups (policy_groups_id, policy_groups_name)
+VALUES (5, 'exercise-location');
+
+INSERT INTO policies (policy_id, policy_key, policy_value, policy_unit, policy_description)
+values (20, 'EXERCISE_LOCATION_MAX_COUNT', '10',
+        'COUNT', '운동 기록이 가질 수 있는 최대 갯수');
+
+
