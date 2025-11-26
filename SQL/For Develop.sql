8b21808a4c551009075cbe1a2248c0974f0d9d52
JooH <nexon9093@gmail.com>
2025-11-26 18:46:24 +0900

    Update For Develop.sql

--- SQL/For Develop.sql
+++ SQL/For Develop.sql
@@ -1,9 +1,1 @@
--- fcm_token_platform 컬럼 추가 (IOS, ANDROID, WEB)
-ALTER TABLE fcm_tokens 
-ADD COLUMN fcm_token_platform VARCHAR(20) NULL COMMENT 'IOS, ANDROID, WEB'
-AFTER fcm_token_value; 
 
--- fcm_token_access_mode 컬럼 추가 (APP, PWA, BROWSER)
-ALTER TABLE fcm_tokens 
-ADD COLUMN fcm_token_access_mode VARCHAR(20) NULL COMMENT 'PWA, BROWSER, APP'
-AFTER fcm_token_platform;
