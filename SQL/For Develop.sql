5dff0948c816fd1502dcad05245aa63d2d5746df
JooH <nexon9093@gmail.com>
2025-12-02 18:19:02 +0900

    Add constraints to fcm_tokens table

--- SQL/For Develop.sql
+++ SQL/For Develop.sql
@@ -1,1 +1,3 @@
+alter table fcm_tokens add constraint chk_fcm_token_platform CHECK (fcm_token_platform IN ('IOS', 'ANDROID', 'PC', 'ETC'));
 
+alter table fcm_tokens add constraint chk_fcm_token_access_mode CHECK (fcm_token_access_mode IN ('APP', 'PWA', 'BROWSER'));
