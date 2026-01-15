c8cbd0e39604a0571928597a969f8e72d7ec1f16
JooH <nexon9093@gmail.com>
2026-01-15 18:35:39 +0900

    Add exercise location policy and mapping

--- SQL/For Main.sql
+++ SQL/For Main.sql
@@ -34,3 +34,11 @@ VALUES ('레슬링', 'https://undabang-public-assets.s3.ap-northeast-2.amazonaws
        ('배드민턴', 'https://undabang-public-assets.s3.ap-northeast-2.amazonaws.com/images/exercises/badminton.png'),
        ('미식축구',
         'https://undabang-public-assets.s3.ap-northeast-2.amazonaws.com/images/exercises/American_football.png');
+
+
+
+INSERT INTO policies (policy_id, policy_key, policy_value, policy_unit, policy_description)
+VALUES (22, 'EXERCISE_LOCATION_MAX_DISTANCE_METER', '5000', 'METERS', '현재 사용자와 운동장소간의 허용 가능한 최대 거리');
+
+INSERT INTO policy_group_mappings (policy_id, policy_groups_id)
+VALUES (22, 5);
