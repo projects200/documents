6053bfe682cac9dd959f8cf07337f1d3ea87a45b
JooH <nexon9093@gmail.com>
2026-01-16 12:06:54 +0900

    Delete policy and mapping insert statements
    Remove insert statements for policy and mapping.

--- SQL/For Develop.sql
+++ SQL/For Develop.sql
@@ -1,6 +1,1 @@
 
-INSERT INTO policies (policy_id, policy_key, policy_value, policy_unit, policy_description)
-VALUES (22, 'EXERCISE_LOCATION_MAX_DISTANCE_METER', '5000', 'METERS', '현재 사용자와 운동장소간의 허용 가능한 최대 거리');
-
-INSERT INTO policy_group_mappings (policy_id, policy_groups_id)
-VALUES (22, 5);
