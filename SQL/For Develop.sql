c0804c2c471d47ac8839f538cd6e143c6876afce
JooH <nexon9093@gmail.com>
2026-01-28 22:10:33 +0900

    Update For Develop.sql

--- SQL/For Develop.sql
+++ SQL/For Develop.sql
@@ -1,8 +1,1 @@
--- 피드 좋아요 인덱스 : (피드ID + 회원ID)
-CREATE INDEX idx_feed_likes_feed_member ON feed_likes (feed_id, member_id);
 
--- 댓글 인덱스 : (피드ID + 회원ID)
-CREATE INDEX idx_comments_feed_member ON comments (feed_id, member_id);
-
--- 댓글 좋아요 인덱스: (댓글ID + 회원ID)
-CREATE INDEX idx_comment_likes_comment_member ON comment_likes (comment_id, member_id);
