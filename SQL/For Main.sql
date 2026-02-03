c76c331a42187dbb80953847f5b6fbd6267b121c
JooH <nexon9093@gmail.com>
2026-02-03 16:05:34 +0900

    Insert policies and group mappings for feed
    Add initial policies and group mappings for feed settings.

--- SQL/For Main.sql
+++ SQL/For Main.sql
@@ -6,3 +6,12 @@ CREATE INDEX idx_comments_feed_member ON comments (feed_id, member_id);
 
 -- 댓글 좋아요 인덱스: (댓글ID + 회원ID)
 CREATE INDEX idx_comment_likes_comment_member ON comment_likes (comment_id, member_id);
+
+INSERT INTO policies (policy_id, policy_key, policy_value, policy_unit, policy_description)
+VALUES (23, 'FEED_PICTURE_MAX_COUNT', '5', 'COUNT', '저장할 수 있는 최대 피드 사진 수');
+
+INSERT INTO policy_groups (policy_groups_id, policy_groups_name)
+VALUES (6, 'feed');
+
+INSERT INTO policy_group_mappings (policy_id, policy_groups_id)
+VALUES (23, 6);
